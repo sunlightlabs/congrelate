005168dab6fa7802150f93c0bfb6c57f49b1a80f
klondike <kprojection@gmail.com>
2009-03-03 14:28:06 -0500

    Fetching senators and representatives from the Sunlight API

--- bigsheet.rb
+++ bigsheet.rb
@@ -1,10 +1,28 @@
 require 'rubygems'
 require 'sinatra'
+require 'sunlight'
+
+include Sunlight
 
 get '/' do
   haml :index
 end
 
 get '/results' do
-  halt 401, "No"
+  
+  if params[:house] == 'representatives'
+    @legislators = REPS
+  elsif params[:house] == 'senators'
+    @legislators = SENATORS
+  else # params[:house] == 'both'
+    @legislators = REPS + SENATORS
+  end
+  
+  haml :results
+end
+
+configure do
+  Sunlight.api_key = 'sunlight9'
+  SENATORS = Legislator.all_where :title => 'Sen'
+  REPS = Legislator.all_where :title => 'Rep'
 end
