d13ad68fe2870e914ad423e4e751cc7595a18f57
klondike <kprojection@gmail.com>
2009-03-05 17:52:23 -0500

    Aded a staging environment

--- environment.rb
+++ environment.rb
@@ -5,8 +5,12 @@ require 'activerecord'
 
 configure :production do
   DATABASE = 'bigsheet_production'
-  Daywalker.api_key = 'sunlight9'
+  Daywalker.api_key = '[make production key]'
 end
+
+configure :staging do
+  DATABASE = 'bigsheet_staging'
+  Daywalker.api_key = 'sunlight9'
 
 configure :development do
   DATABASE = 'bigsheet_dev'
@@ -15,7 +19,7 @@ end
 
 configure :test do
   DATABASE = 'bigsheet_test'
-  Daywalker.api_key = 'sunlight9'
+  Daywalker.api_key = '[tests should not hit any remote API]'
 end
 
 ActiveRecord::Base.establish_connection(
