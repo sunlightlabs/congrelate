5c66879a45cee2a501b75469b9c974a50653ac56
klondike <kprojection@gmail.com>
2009-03-26 14:40:13 -0400

    Moved database details out into YML, with an example file

--- config/environment.rb
+++ config/environment.rb
@@ -6,38 +6,41 @@ require 'activerecord'
 require 'hpricot'
 require 'fastercsv'
 
+def database(environment)
+  details = YAML.load_file('config/database.yml')[environment.to_s]
+  ActiveRecord::Base.establish_connection(
+    :adapter => 'mysql',
+    :reconnect => true,
+    :username => details[:username],
+    :password => details[:password],
+    :database => details[:database],
+    :host => details[:host]
+  )
+end
+
 configure :production do
-  DATABASE = 'bigsheet_production'
   Daywalker.api_key = '[make production key]'
   OPENSECRETS_API_KEY = 'e38f1739902531476d7d450ab966a3a7' # infinite
 end
 
 configure :staging do
-  DATABASE = 'bigsheet_staging'
   Daywalker.api_key = 'sunlight9'
   OPENSECRETS_API_KEY = 'e38f1739902531476d7d450ab966a3a7' # infinite
 end
 
 configure :development do
-  DATABASE = 'bigsheet_dev'
   Daywalker.api_key = 'sunlight9'
   OPENSECRETS_API_KEY = 'd55ecab9fdc27683d3512af9e2c4deb5' # limited
 end
 
 configure :test do
-  DATABASE = 'bigsheet_test'
   Daywalker.api_key = '[tests should not hit any remote API]'
   OPENSECRETS_API_KEY = '[tests should not hit any remote API]'
 end
 
-ActiveRecord::Base.establish_connection(
-  :adapter => 'mysql',
-  :username => 'bigsheet',
-  :password => 'bigsheet',
-  :database => DATABASE,
-  :reconnect => true,
-  :host => 'localhost'
-)
+configure do
+  database Sinatra::Application.options.environment
+end
 
 def load_models
   Dir.glob('sources/**/*.rb').each {|model| load model}
