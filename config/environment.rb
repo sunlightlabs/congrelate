b0495d7c73f03c2938dd4dd8dd3831cdcee0b24b
klondike <kprojection@gmail.com>
2009-05-04 16:06:57 -0700

    Updated views directory

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,6 @@ require 'sinatra'
 gem 'activerecord', '>=2.3'
 require 'activerecord'
 
-set :views, File.dirname(__FILE__) + "/.."
-
 class Source < ActiveRecord::Base; end
 class Update < ActiveRecord::Base; validates_presence_of :status, :source, :elapsed_time; end
 
@@ -18,7 +16,9 @@ configure do
     :database => details[:database],
     :host => details[:host]
   )
-  
+end
+
+def load_sources
   api_keys = YAML.load_file('sources/api_keys.yml')[Sinatra::Application.environment]
   Dir.glob('sources/*').reject {|file| file.include? '.yml'}.each do |dir|
     Dir.glob(File.join(dir, '*.rb')).each {|model| load model}
