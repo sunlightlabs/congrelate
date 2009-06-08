0bc192579ccf5704f005edeb479c98b41b47d9a3
Luigi Montanez <luigi.montanez@gmail.com>
2009-06-08 17:41:11 -0400

    Add some notes regarding issues with initial rake db:migrate.

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,15 @@ require 'sinatra'
 gem 'activerecord', '>=2.3'
 require 'activerecord'
 
-class Source < ActiveRecord::Base; end
-class Update < ActiveRecord::Base; validates_presence_of :status, :source, :elapsed_time; end
-
 configure do
   details = YAML.load_file('config/database.yml')[Sinatra::Application.environment]
   ActiveRecord::Base.establish_connection(details.merge(:reconnect => true))
 end
 
+### Below can't be run until initial rake db:migrate
+
+class Source < ActiveRecord::Base; end
+class Update < ActiveRecord::Base; validates_presence_of :status, :source, :elapsed_time; end
 
 api_keys = YAML.load_file('sources/api_keys.yml')[Sinatra::Application.environment]
 sources = Source.all
