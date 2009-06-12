67caf0376069fb7a456228e7d5458824558df3e1
klondike <kprojection@gmail.com>
2009-06-12 10:30:55 -0400

    Fixed bug in last refactoring

--- config/bootstrap_environment.rb
+++ config/bootstrap_environment.rb
@@ -6,6 +6,6 @@ require 'activerecord'
 configure do
   details = YAML.load_file 'config/database.yml'
   if details and details[Sinatra::Application.environment]
-    ActiveRecord::Base.establish_connection details.merge(:reconnect => true)
+    ActiveRecord::Base.establish_connection details[Sinatra::Application.environment].merge(:reconnect => true)
   end
 end
