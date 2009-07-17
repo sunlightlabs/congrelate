6f47da411deb7d14be4d3a07da200b6f01bdfb1a
klondike <kprojection@gmail.com>
2009-07-17 17:32:09 -0400

    Enhanced example source class documentation

--- sources/example/example.rb
+++ sources/example/example.rb
@@ -9,15 +9,37 @@ class Example < ActiveRecord::Base
   # Populate the column data for this field
   def self.field_for(legislators, column)
     
+    # Return a hash indexed by a legislator's bioguide_id
     field = {}
     
     legislators.each do |legislator|
-      field[legislator.bioguide_id] = value_related_to_this_column
+      field[legislator.bioguide_id] = case column
+      when 'name'
+        # For each legislator, you can return a scalar value for the column...
+        'John Adams'
+      when 'full name'
+        # ...or you can use a hash to return one value for the API, and one for the frontend...
+        {
+          :html => 'John <strong>Quincy</strong> Adams',
+          :data => 'John Quincy Adams'
+        }
+      when 'skill'
+        # ...and you can use a hash to return hidden text that will be searchable by the filter.
+        # The frontend will use the :data parameter if no :html parameter is given.
+        {
+          :data => 'BB',
+          :searchable => 'Black Belt'
+        }
+      end
     end
 
+    # You can give the column as a whole a few special optional attributes...
+    
+    # The text in the <th> field for that column (defaults to a titleizeation of the column name)
     field[:header] = "COLUMN HEADER"
+    # The tooltip text for the <th> field (defaults to a titleizeation of the column name)
     field[:title] = "MOUSEOVER TEXT"
-    field[:type] = "string" # or digit or currency
+    field[:type] = "string" # or digit or currency, to force a certain kind of sorting
     field
   end
   
@@ -31,7 +53,9 @@ class Example < ActiveRecord::Base
   #     3. Store it into the local database
   #
   # Make sure to create an entry in sources.yml for the data source
+  
   def self.update(options = {})
+    # Return a 2-item array, with SUCCESS or FAILURE, followed by the status message
     ['SUCCESS', "Message on success"]
   end
 
